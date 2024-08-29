0d5799e834ea4c4ba9804f7f8448ae6142bb6f2f
Solemark <larco9mason@gmail.com>
2024-08-29 17:33:19 +1000

    simplify reverse_array

--- console_apps/src/reverse_array.rs
+++ console_apps/src/reverse_array.rs
@@ -1,20 +1,14 @@
 #[allow(dead_code)]
-pub fn same_arr<N>(mut input: Vec<N>) -> Vec<N>
-where
-    N: Copy,
-{
-    let mut x: N;
-    let mut y: N;
-    let mut i: usize = 0;
-    let mut c: usize = input.len() - 1;
-
-    while i <= c {
-        x = input[i];
-        y = input[c];
-        input[c] = x;
+pub fn same_arr<N: Clone>(mut input: Vec<N>) -> Vec<N> {
+    let mut i = 0;
+    let mut j = input.len() - 1;
+    while i <= j {
+        let x = input[i].clone();
+        let y = input[j].clone();
+        input[j] = x;
         input[i] = y;
         i += 1;
-        c -= 1;
+        j -= 1;
     }
     input
 }
--- console_apps/src/test/reverse_array_test.rs
+++ console_apps/src/test/reverse_array_test.rs
@@ -4,9 +4,9 @@ mod tests {
 
     #[test]
     fn test_reverse_samearray() {
-        let input: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let expect: Vec<i32> = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-        let result: Vec<i32> = same_arr(input);
+        let input = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let expect = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+        let result = same_arr(input);
         assert_eq!(expect, result)
     }
 }
