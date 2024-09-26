091e4dadc5e78e3dd3241d602c0926fc28046cfd
Solemark <larco9mason@gmail.com>
2024-09-26 20:06:59 +1000

    fix json_formatter not jsonifying

--- console_apps/src/test/json_formatter_test.rs
+++ console_apps/src/test/json_formatter_test.rs
@@ -5,7 +5,7 @@ mod tests {
     #[test]
     fn test_str_to_json() {
         assert_eq!(
-            String::from("\"message\": \"Hello World!\""),
+            String::from("{\"message\": \"Hello World!\"}"),
             str_to_json(String::from("message"), String::from("Hello World!"))
         );
     }
